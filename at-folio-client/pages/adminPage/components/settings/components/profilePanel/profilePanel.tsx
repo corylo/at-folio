f40f6acdea32f5f5b6686c436cb7ab0d23f5a609
Cory Loeffelman <frontenddevemail@gmail.com>
2021-11-04 14:03:40 -0500

    Added profile url actions

--- at-folio-client/pages/adminPage/components/settings/components/profilePanel/profilePanel.tsx
+++ at-folio-client/pages/adminPage/components/settings/components/profilePanel/profilePanel.tsx
@@ -1,4 +1,5 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 import { Input } from "../../../../../../components/input/input";
 import { PhotoPickerToggle } from "../../../../../../components/photoPickerToggle/photoPickerToggle";
@@ -11,10 +12,34 @@ import { SettingsContext } from "../../settingsWrapper";
 import { SettingsPanelOption } from "../../enums/settingsPanelOption";
 
 export const ProfilePanel: React.FC = () => {
-  const { appState } = useContext(AppContext),
+  const { profile } = useContext(AppContext),
     { setOptionTo } = useContext(SettingsContext);
 
-  const { profile } = appState;
+  const [copied, setCopiedTo] = useState<boolean>(false);
+
+  useEffect(() => {
+    const timeout: NodeJS.Timeout = setTimeout(() => {
+      setCopiedTo(false);
+    }, 2000);
+
+    return () => {
+      clearTimeout(timeout);
+    }
+  }, [copied]);
+
+  const url: string = `https://atfol.io/${profile.username}`;
+
+  const handleOnCopy = async (): Promise<void> => {
+    if(navigator.clipboard) {
+      try {
+        await navigator.clipboard.writeText(url)
+
+        setCopiedTo(true);
+      } catch (err) {
+        console.error(err);
+      }
+    }
+  }
 
   return (
     <SettingsPanel id="profile-panel" icon="fa-regular fa-user" title="Profile">
@@ -24,10 +49,25 @@ export const ProfilePanel: React.FC = () => {
             disabled
             type="text" 
             placeholder="Enter username" 
-            value={`https://atfol.io/${profile.username}`}
+            value={url}
             onChange={() => {}}
           />
         </Input>          
+        <div className="url-section-actions">
+          <button
+            type="button"
+            className="url-section-action button rubik-font"
+            onClick={handleOnCopy}
+          >
+            { copied ? "Copied" : "Copy" }
+          </button>
+          <Link
+            to={`/${profile.username}`}
+            className="url-section-action link rubik-font"
+          >
+            View
+          </Link>
+        </div>
       </SettingsSection>
       <SettingsSection className="username-section" label="Username">
         <Input>
