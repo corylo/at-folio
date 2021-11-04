82d0660519b65c6f9382c40f7484b9993ce3f003
Cory Loeffelman <frontenddevemail@gmail.com>
2021-11-04 13:35:19 -0500

    Updated brand statement

--- at-folio-client/components/brandStatement/brandStatement.tsx
+++ at-folio-client/components/brandStatement/brandStatement.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export const BrandStatement: React.FC = () => {  
   return (       
     <div className="brand-statement">
-      <h1 className="rubik-font">A link in bio service that <span className="highlight">has style</span>.</h1>
+      <h1 className="rubik-font">A minimalist link in bio service that <span className="highlight">looks good</span>.</h1>
     </div>
   );
 }
