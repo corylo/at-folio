e7583cb23a679ad8592901cbd24d3bce388895aa
Cory Loeffelman <frontenddevemail@gmail.com>
2021-12-07 10:55:29 -0600

    Added type in algolia service

--- at-folio-client/services/algoliaService.ts
+++ at-folio-client/services/algoliaService.ts
@@ -4,17 +4,19 @@ import { algoliaConfig } from "../../config/algolia";
 
 import { AlgoliaUtility } from "../utilities/algoliaUtility";
 
+import { IProfileSearchResult } from "../../at-folio-models/profileSearchResult";
+
 import { FirestoreCollectionID } from "../../at-folio-enums/firestoreCollectionID";
 
 const Algolia: SearchClient = AlgoliaLite(algoliaConfig.appID, algoliaConfig.apiKey),
   ProfileIndex: SearchIndex = Algolia.initIndex(FirestoreCollectionID.Profiles);
 
 interface IAlgoliaService {
-  search: (query: string) => Promise<any[]>;
+  search: (query: string) => Promise<IProfileSearchResult[]>;
 }
 
 export const AlgoliaService: IAlgoliaService = {
-  search: async (query: string): Promise<any[]> => {
+  search: async (query: string): Promise<IProfileSearchResult[]> => {
     const res: any = await ProfileIndex.search(query, { hitsPerPage: 5 });
 
     return AlgoliaUtility.mapResults(FirestoreCollectionID.Profiles, res.hits);
